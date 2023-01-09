9b1c30c68b04b751ca6839ffb111bcf1aa6489df
rstott87 <rstott87@yahoo.com>
2023-01-09 16:58:59 -0500

    changes

--- api/server.js
+++ api/server.js
@@ -1,5 +1,17 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+
+const projectsRouter = require("./projects/projects-router");
+
+server.use(express.json());
+
+server.use("./api/projects", projectsRouter);
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "notddd found"
+  });
+});
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
